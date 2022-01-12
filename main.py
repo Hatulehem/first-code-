f9d4a479e3e65b1ff0b54a78ec357bd6319f82df
Hatulehem <97167068+Hatulehem@users.noreply.github.com>
2022-01-12 13:27:33 +0200

    main

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("my breaded cat !!")
+import pygame
+import time
+
+# screen size 
+WINDOW_W = 1360
+WINDOW_H = 1080
+WINDOW_SIZE = (WINDOW_W, WINDOW_H)
+
+# BK_COLOR = (205,255,255)
+
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Hypnosis mic: Alternative Rap Battle")
+
+img = pygame.image.load("dotsuirate hompo12.jpg")
+
+play = True
+while play:
+  # screen.fill(BK_COLOR)
+  screen.blit(img,(0,0))
+  pygame.display.flip()
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      play = False
+
+pygame.quit()
